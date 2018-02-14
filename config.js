1ad2ba87b12b11bbcd3f5e2e87c2492f69f877a8
Dave Gentry <davegentry85@gmail.com>
2018-02-14 15:58:16 -0500

    deleting config file

--- config.js
+++
@@ -1,9 +0,0 @@
-//Twitter API info
-
-module.exports = {
-    consumer_key:         'IDE1Shcn5dwYKPjH1kaA6L9q7',
-  consumer_secret:      'HJm8x8Llb8n1jFf6yCdfeFxhooScSFyQHOwhaEKdaHMxFVl2W7',
-  access_token:         '831902731010969601-XFYpArrQwCf2dXNCGSVbWi9eB9FOgcU',
-  access_token_secret:  'hhM7P3FsJxaQXLGCR2OjYGrN6rT48IZf5R9h2emoXq0Rv'
-     
-}
